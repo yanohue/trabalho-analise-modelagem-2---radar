64267946ed01688498780620cc8aaf0c24de92a7
yanohue <108890917+yanohue@users.noreply.github.com>
2023-11-12 12:01:30 -0300

    Added localTime

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Sensor sensor = new Sensor();
@@ -5,6 +9,16 @@ public class App {
 
         Speedometer speedometer = new Speedometer();
         float speed = speedometer.getSpeed(measurement[0], measurement[1]);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatedDateTime = localDateTime.format(format);
+
+        LocalTime preciseTime = LocalTime.now();
+
+        System.out.println(formatedDateTime);
+        System.out.println(preciseTime);
+
 
         System.out.println(measurement[0] + " ms");
         System.out.println(measurement[1] + " ms");
